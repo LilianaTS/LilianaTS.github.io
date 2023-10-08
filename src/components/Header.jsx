aa2960d4a934b7456e7062a53691cf87d693fb5f
Liliana Santos <80032737+LilianaTS@users.noreply.github.com>
2023-10-08 04:07:20 +0100

    Header Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import picture from "../picture.jpg"
+import picture from "../picture.jpg";
 
 function Header() {
-  return (
-    <header className="App-header">
-        <img src={picture} className="picture" alt="Liliana Santos"/> 
-        <h1 className='title'>Liliana Santos<br/>Junior Software Developer</h1>       
-    </header>
-  )
+	return (
+		<header className="App-header">
+			<img src={picture} className="picture" alt="Liliana Santos" />
+			<h1 className="title">Liliana Santos</h1>
+			<br />
+			<h2 className="subtitle">Junior Software Developer</h2>
+		</header>
+	);
 }
 
-export default Header
+export default Header;
